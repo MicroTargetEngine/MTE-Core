45e584b943699e86268dba754d9296d7c3c59a81
Doohoon Kim <invi.dh.kim@gmail.com>
2016-10-21 04:17:33 +0900

    Add log for Debug to SyncSignal initialize function.

--- src/common/thread/Thread.cpp
+++ src/common/thread/Thread.cpp
@@ -37,6 +37,10 @@
 
 #if defined(_Thread_hpp_)
 
+#if defined(LOG_WRITE_MODE)
+#include "LogD.hpp"
+#endif
+
 SyncSignal::SyncSignal() {
   _Initialize();
 }
@@ -46,16 +50,31 @@ SyncSignal::~SyncSignal() {
 }
 
 void SyncSignal::_Initialize() {
+#if defined(LOG_WRITE_MODE)
+  G_LogD->Logging("Func", "into SyncSignal _Initialize Function");
+#endif
   __CONDINIT(_Cond);
+#if defined(LOG_WRITE_MODE)
+  G_LogD->Logging("Var", "into SyncSignal __CONDINIT");
+#endif
 #if defined(SET_LIB_PTHREAD)
   __MUTEXINIT(_Mutex);
+#if defined(LOG_WRITE_MODE)
+  G_LogD->Logging("Var", "into SyncSignal __MUTEXINIT");
+#endif
 #elif defined(WINDOWS_SYS) && !defined(SET_LIB_PTHREAD)
   __CSINIT(_CriticalSection);
+  #if defined(LOG_WRITE_MODE)
+  G_LogD->Logging("Var", "into SyncSignal __CSINIT");
+#endif
 #endif
   _IsWaiting = false;
 }
 
 void SyncSignal::_Deinitialize() {
+#if defined(LOG_WRITE_MODE)
+  G_LogD->Logging("Func", "into SyncSignal _Deinitialize Function");
+#endif
   __CONDDESTROY(_Cond);
 #if defined(SET_LIB_PTHREAD)
   __MUTEXDESTROY(_Mutex);
